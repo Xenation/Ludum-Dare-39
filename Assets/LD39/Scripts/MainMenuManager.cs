ad00ffc40413b96ed691200e7f3d956e7247d2fa
Xenation <xenations.111@gmail.com>
2017-08-01 03:47:17 +0200

    Fixes

---
+++ Assets/LD39/Scripts/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿namespace LD39 {
+	public class MainMenuManager : Singleton<MainMenuManager> {
+
+		public void Start() {
+			
+		}
+
+		public void StartBtn() {
+			DifficultyManager.I.NextLevel();
+		}
+
+		public void QuitBtn() {
+			DifficultyManager.I.ExitGame();
+		}
+
+	}
+}
